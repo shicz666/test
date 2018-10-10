f57f10323da360a430f465af0c6540882129d209
shicz <1038901688@qq.com>
2018-10-10 14:57:24 +0800

    fdsfdsfsd

---
+++ src/main/java/com/scz/Test.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.scz;
+
+/**
+ * @author ecom_wm
+ *
+ */
+public class Test {
+        public static void main(String[] args) {
+			String aString="°¡Èö¿ªÁË¶à¾Ã";
+		}   
+}
