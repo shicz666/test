331ed2fd8c613929f09f894b379c528e444d17bb
shicz666 <18710279538@163.com>
2018-10-11 17:10:16 +0800

    密码吗

--- src/main/java/com/scz/Test.java
+++ src/main/java/com/scz/Test.java
@@ -12,7 +12,6 @@ public class Test {
 			String aString="啊撒开了多久";
 			String sdjskString="的";
 			String sdadfdsadaString="大萨达阿斯蒂芬发送到";
-                        String sdsdsadsa="困死了较大喀什as";
-		        String sakdjsak="哈哈哈哈哈";
+            String dsadsa="困死了较大喀什as";
 		}   
 }
