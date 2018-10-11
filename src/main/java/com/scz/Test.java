acd8c727d4e51212f7b7a9365ff53385f91b55ae
shicz666 <18710279538@163.com>
2018-10-11 17:57:40 +0800

    了解冲突

--- src/main/java/com/scz/Test.java
+++ src/main/java/com/scz/Test.java
@@ -11,8 +11,8 @@ public class Test {
         public static void main(String[] args) {
 			String aString="啊撒开了多久";
 			String sdjskString="的";
-			String sdadfdsadaString="大萨达阿斯蒂芬发送到";
-	    String lll="小鑫鑫";
+			String sdadfdsadaString="x";
+	        String lll="小鑫鑫6666";
             String dsadsa="困死了较大喀什as1111";
             int i=1111;
             int l=6111;
